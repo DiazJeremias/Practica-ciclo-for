e41f8b93717b672f4aa440a6b4bf3a32ad0a1860
Jeremias <diazjeremias95@gmail.com>
2024-04-19 13:43:09 -0300

    Primera subida

---
+++ for-ejercicio3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace for_ejercicio3
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               //Hacer un programa que solicite 20 edades y luego calcule el promedio de edad de aquellas personas mayores a 18 años.
+
+               int edad;
+               float promedio, acu = 0, cont = 0;
+
+               for (int x = 0; x < 20; x++)
+               {
+                    Console.WriteLine("Ingrese una edad:");
+                    edad = int.Parse(Console.ReadLine());
+
+                    if (edad > 18)
+                    {
+                         acu += edad;
+                         cont++;
+                    }
+               }
+
+               promedio = acu / cont;
+
+               Console.WriteLine("El promedio de edad de los mayores de 18 años es " + promedio.ToString("0.00"));
+          }
+     }
+}
---
+++ for-ejercicio5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace for_ejercicio5
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               //Hacer un programa que solicite 20 números y luego emitir por pantalla el máximo de los números pares y el mínimo de los números impares.
+
+               int n, max = 0, min = 0;
+               bool bpar = false, bimpar = false;
+
+               for (int x = 0; x < 20; x++)
+               {
+                    Console.WriteLine("Ingrese un numero:");
+                    n = int.Parse(Console.ReadLine());
+
+                    if (n % 2 == 0)
+                    {
+                         if (!bpar)
+                         {
+                              max = n;
+                              bpar = true;
+                         }
+                         else if (n > max)
+                         {
+                              max = n;
+                         }
+                    }
+                    else if (!bimpar)
+                    {
+                         min = n;
+                         bimpar = true;
+                    }
+                    else if (n < min)
+                    {
+                         min = n;
+                    }
+               }
+
+               Console.WriteLine("El maximo de los pares es: " + max);
+               Console.WriteLine("El minimo de los impares es: " + min);
+          }
+     }
+}
