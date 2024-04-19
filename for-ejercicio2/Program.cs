4728403d6c56a8f62b256c10f607929ebb32b5e9
Jeremias <diazjeremias95@gmail.com>
2024-04-19 13:41:41 -0300

    Primera subida

---
+++ for-ejercicio2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace for_ejercicio2
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               //Hacer un programa que solicite el ingreso de 10 números y que muestre el mayor de ellos por pantalla. Solo se debe emitir UN valor por pantalla.
+
+               // int n, mayor;
+
+               // Console.WriteLine("Ingrese un numero:");
+               // n = int.Parse(Console.ReadLine());
+
+               // mayor = n;
+
+               // for (int x = 0; x < 9; x++)
+               // {
+               //      Console.WriteLine("Ingrese un numero:");
+               //      n = int.Parse(Console.ReadLine());
+
+               //      if (n > mayor)
+               //      {
+               //           mayor = n;
+               //      }
+               // }
+
+               // Console.WriteLine("El mayor de los numeros es " + mayor);
+
+               int n, mayor = 0;
+
+               for (int x = 0; x < 10; x++)
+               {
+                    Console.WriteLine("Ingrese un numero:");
+                    n = int.Parse(Console.ReadLine());
+
+                    if (x == 0)
+                    {
+                         mayor = n;
+                    }
+                    else if (n > mayor)
+                    {
+                         mayor = n;
+                    }
+               }
+
+               Console.WriteLine("El mayor de los numeros es " + mayor);
+          }
+     }
+}
